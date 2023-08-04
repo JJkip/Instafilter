c53aa154e5f68ed79b1bfb076de177583c598660
jjkip <jjlangatjose@gmail.com>
2023-08-04 22:29:04 +0300

    Showing multiple options with confirmationDialog() in SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
             print("New value is \(blurAmount)")
         }
     }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         VStack {
             Text("Blurred Text")
@@ -23,6 +26,21 @@ struct ContentView: View {
             Button("Random Blur"){
                 blurAmount = Double.random(in: 0...20)
             }
+            
+            Text("Click to show confirmation dialog")
+                .frame(width:300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red}
+                    Button("Green") { backgroundColor = .green}
+                    Button("Blue") { backgroundColor = .blue}
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                     Text("Select a new color")
+                }
         }
         .onChange(of: blurAmount) { newValue in
             print("New value is \(newValue)")
