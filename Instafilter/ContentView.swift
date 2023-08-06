ea23f9d7c3933e868cd55c45d028fb5c35ee87bd
jjkip <jjlangatjose@gmail.com>
2023-08-06 23:01:02 +0300

    Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -67,13 +67,25 @@ struct ContentView: View {
 //        let currentFilter = CIFilter.sepiaTone()
 //        let currentFilter = CIFilter.pixellate()
 //        let currentFilter = CIFilter.crystallize()
+//        let currentFilter = CIFilter.sepiaTone()
         let currentFilter = CIFilter.twirlDistortion()
         currentFilter.inputImage = beginImage
 //        currentFilter.intensity = 1
 //        currentFilter.scale = 100
 //        currentFilter.radius = 200
-        currentFilter.radius = 1000
-        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+//        currentFilter.radius = 1000
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
         
         // get a CIImage from our filter or exit if that fails
         guard let outputImage = currentFilter.outputImage else { return }
