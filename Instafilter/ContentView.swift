377d1fd04d37200cc6974a123f59cefce3489337
jjkip <jjlangatjose@gmail.com>
2023-08-10 23:17:26 +0300

    Building  Instafilter basic SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -20,119 +20,167 @@ struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            Button("Select Image"){
-                showingImagePicker = true
-            }
-            Button("Save Image"){
-                guard let inputImage = inputImage else { return }
-                
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
-        }
-        .sheet(isPresented: $showingImagePicker){
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadSelectedImage() }
-        /*
-        VStack {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill()
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
                     image?
                         .resizable()
                         .scaledToFit()
                 }
-                .onAppear(perform: loadImage)
-         */
-        /*
-        VStack {
-            
-            Text("Blurred Text")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
-                
-            Button("Random Blur"){
-                blurAmount = Double.random(in: 0...20)
-            }
-            
-            Text("Click to show confirmation dialog")
-                .frame(width:300, height: 300)
-                .background(backgroundColor)
                 .onTapGesture {
-                    showingConfirmation = true
+                    // select an image
                 }
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") { backgroundColor = .red}
-                    Button("Green") { backgroundColor = .green}
-                    Button("Blue") { backgroundColor = .blue}
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                     Text("Select a new color")
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
                 }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save", action: save) 
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .onChange(of: blurAmount) { newValue in
-            print("New value is \(newValue)")
-        }
-        .padding()
-        */
     }
     
-    func loadSelectedImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+    func save(){
         
-//        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
     }
+    /*
+     VStack {
+     image?
+     .resizable()
+     .scaledToFit()
+     Button("Select Image"){
+     showingImagePicker = true
+     }
+     Button("Save Image"){
+     guard let inputImage = inputImage else { return }
+     
+     let imageSaver = ImageSaver()
+     imageSaver.writeToPhotoAlbum(image: inputImage)
+     }
+     }
+     .sheet(isPresented: $showingImagePicker){
+     ImagePicker(image: $inputImage)
+     }
+     .onChange(of: inputImage) { _ in loadSelectedImage() }
+     */
     
-    func loadImage() {
-        guard let inputImage = UIImage(named: "Movies") else { return }
-           let beginImage = CIImage(image: inputImage)
-//            image = Image("Movies")
-        let context = CIContext()
-//        let currentFilter = CIFilter.sepiaTone()
-//        let currentFilter = CIFilter.pixellate()
-//        let currentFilter = CIFilter.crystallize()
-//        let currentFilter = CIFilter.sepiaTone()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-//        currentFilter.scale = 100
-//        currentFilter.radius = 200
-//        currentFilter.radius = 1000
-//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-        
-        let amount = 1.0
-
-        let inputKeys = currentFilter.inputKeys
-
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-        
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
-        
-        // get a CIImage from our filter or exit if that fails
-        guard let outputImage = currentFilter.outputImage else { return }
-
-        // attempt to get a CGImage from our CIImage
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            // convert that to a UIImage
-            let uiImage = UIImage(cgImage: cgimg)
-
-            // and convert that to a SwiftUI image
-            image = Image(uiImage: uiImage)
+    /*
+     VStack {
+     image?
+     .resizable()
+     .scaledToFit()
+     }
+     .onAppear(perform: loadImage)
+     */
+    /*
+     VStack {
+     
+     Text("Blurred Text")
+     .blur(radius: blurAmount)
+     
+     Slider(value: $blurAmount, in: 0...20)
+     
+     Button("Random Blur"){
+     blurAmount = Double.random(in: 0...20)
+     }
+     
+     Text("Click to show confirmation dialog")
+     .frame(width:300, height: 300)
+     .background(backgroundColor)
+     .onTapGesture {
+     showingConfirmation = true
+     }
+     .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+     Button("Red") { backgroundColor = .red}
+     Button("Green") { backgroundColor = .green}
+     Button("Blue") { backgroundColor = .blue}
+     Button("Cancel", role: .cancel) { }
+     } message: {
+     Text("Select a new color")
+     }
+     }
+     .onChange(of: blurAmount) { newValue in
+     print("New value is \(newValue)")
+     }
+     .padding()
+     */
+    
+    /*
+     func loadSelectedImage() {
+     guard let inputImage = inputImage else { return }
+     image = Image(uiImage: inputImage)
+     
+     //        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+     }
+     */
+    /*
+     func loadImage() {
+     guard let inputImage = UIImage(named: "Movies") else { return }
+     let beginImage = CIImage(image: inputImage)
+     //            image = Image("Movies")
+     let context = CIContext()
+     //        let currentFilter = CIFilter.sepiaTone()
+     //        let currentFilter = CIFilter.pixellate()
+     //        let currentFilter = CIFilter.crystallize()
+     //        let currentFilter = CIFilter.sepiaTone()
+     let currentFilter = CIFilter.twirlDistortion()
+     currentFilter.inputImage = beginImage
+     //        currentFilter.intensity = 1
+     //        currentFilter.scale = 100
+     //        currentFilter.radius = 200
+     //        currentFilter.radius = 1000
+     //        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+     
+     let amount = 1.0
+     
+     let inputKeys = currentFilter.inputKeys
+     
+     if inputKeys.contains(kCIInputIntensityKey) {
+     currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+     
+     if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+     
+     if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+     
+     // get a CIImage from our filter or exit if that fails
+     guard let outputImage = currentFilter.outputImage else { return }
+     
+     // attempt to get a CGImage from our CIImage
+     if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+     // convert that to a UIImage
+     let uiImage = UIImage(cgImage: cgimg)
+     
+     // and convert that to a SwiftUI image
+     image = Image(uiImage: uiImage)
+     }
+     }
+     }
+     */
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-        }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
